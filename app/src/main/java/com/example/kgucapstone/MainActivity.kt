49a111f86b2c68e745a13a10b3c43776f2d7e089
Shine <zero777960@gmail.com>
2025-05-26 03:32:30 +0900

    2025-05-26 Gemini api 및 레이아웃 구현

--- app/src/main/java/com/example/kgucapstone/MainActivity.kt
+++ app/src/main/java/com/example/kgucapstone/MainActivity.kt
@@ -5,12 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.launch
+import androidx.lifecycle.lifecycleScope
+
+import android.widget.TextView
+
+import com.google.firebase.Firebase
+import com.google.firebase.ai.GenerativeModel
+import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.GenerativeBackend
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val responseTextView = findViewById<TextView>(R.id.test)
+
+        // Initialize the Gemini Developer API backend service
+        // Create a `GenerativeModel` instance with a model that supports your use case
+        val model = Firebase.ai(backend = GenerativeBackend.googleAI())
+            .generativeModel("gemini-2.0-flash")
+
+        // Provide a prompt that contains text
+        val prompt = "안녕? 나는 경기대학교에서 캡스톤디자인 프로젝트를 하고 있는 대학생이야. 너는 나의 AI 도우미야. 내가 질문하면 대답해줘."
+
+        // To generate text output, call generateContent with the text input
+        lifecycleScope.launch {
+            val response = model.generateContent(prompt)
+
+            responseTextView.text = response.text
+        }
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
